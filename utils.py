d9d79a506a1db860d9381646f67edbd514b8b96a
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-09-28 22:14:55 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -150,9 +150,11 @@ def get_hparams(init=True):
                       help='JSON file for configuration')
   parser.add_argument('-m', '--model', type=str, required=True,
                       help='Model name')
+  parser.add_argument('-l', '--log_dir', type=str, default="./logs",
+                      help='log dir')
   
   args = parser.parse_args()
-  model_dir = os.path.join("./logs", args.model)
+  model_dir = os.path.join(args.log_dir, args.model)
 
   if not os.path.exists(model_dir):
     os.makedirs(model_dir)
